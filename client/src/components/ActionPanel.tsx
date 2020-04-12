7957cabe52e2f6ed4aa31e90683c2ef68383c4a9
Eric <Eric@Erics-MacBook-Pro.local>
2020-04-12 07:35:44 -0400

    Refactored WagerInput

--- client/src/components/ActionPanel.tsx
+++ client/src/components/ActionPanel.tsx
@@ -1,48 +1,18 @@
 import React from 'react'; 
 import Message from '../interfaces/Message';
 import GameData from '../interfaces/GameData';
-import PlayerData from '../interfaces/PlayerData';
+import WagerInput from './WagerInput';
 
 type myProps = {
   messagesSender: any,
-  gameData: GameData,
-  playerData: PlayerData
+  gameData: GameData
 };
 
-type myState = {
-  minDiceNum: number,
-  minDiceQty: number,
-  currentNum: number,
-  currentQty: number,
-};
-
-export default class ActionPanel extends React.Component<myProps, myState> {
+export default class ActionPanel extends React.Component<myProps> {
   constructor(props: myProps) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleReset = this.handleReset.bind(this);
-    this.changeWagerNum = this.changeWagerNum.bind(this);
-  }
-
-  componentDidMount() {
-    if (this.props.gameData.lastWager.num && this.props.gameData.lastWager.qty) {
-      let minNum = this.props.gameData.lastWager.num !== 6 ? this.props.gameData.lastWager.num + 1 : 1;
-      let minQty = this.props.gameData.lastWager.num !== 6 ? this.props.gameData.lastWager.qty : this.props.gameData.lastWager.qty + 1;
-      
-      this.setState({ 
-        minDiceNum: minNum,
-        minDiceQty: minQty,
-        currentNum: minNum, 
-        currentQty: minQty
-      });
-    } else {
-      this.setState({ 
-        minDiceNum: 1, 
-        minDiceQty: 1,
-        currentNum: 1,
-        currentQty: 1
-      });
-    }
   }
 
   private handleSubmit() {
@@ -55,73 +25,17 @@ export default class ActionPanel extends React.Component<myProps, myState> {
     this.props.messagesSender(message);
   }
 
-  private changeWagerNum(direction: 'increase' | 'decrease') {
-    let currentNum = this.state.currentNum;
-    let currentQty = this.state.currentQty;
-    let minQty = this.state.minDiceQty;
-    let minNum = this.state.minDiceNum;
-    let diceRemaining = this.props.gameData.numDiceRemaining;
-
-    if (direction==='increase') {
-      switch (true) {
-        case currentNum < 6:
-          this.setState({currentNum: currentNum + 1});
-          break;
-        case currentNum === 6:
-          // Allow the + button to rollover from 6 to 1, but increase the wager qty
-          // Only allow legal wagers
-          if (currentQty < diceRemaining) {
-            this.setState({ currentNum: 1, currentQty: currentQty + 1 });
-          }
-          break;
-      }
-    } else {
-      switch (true) {
-        case currentNum > 1:
-          //Only allow legal wagers
-          if (currentNum > minNum && currentQty >= minQty) {
-            this.setState({ currentNum: currentNum - 1 });
-          }
-          break;
-        case currentNum === 1:
-          // Allow the - button to rollowver from 1 to 6, but decrease the wager qty
-          // Only allow legal wagers
-          if (minQty < currentQty) {
-            this.setState({ currentNum: 6, currentQty: currentQty - 1 });
-          }
-          break;
-      }
-    }
-  }
-
-  private GameDice = (props: { num: Number }) => {
-    if (props.num===0) { return(<span></span>); }
-    return (
-      <img 
-        src={"../images/dice"+ props.num.toString() + ".svg"} 
-        alt={props.num.toString()} 
-        height="40px" 
-        className="img-dice">
-      </img>
-    );
-  }
-
   render() {
-    // TODO: Finish rendering the action panel
     if (this.props.gameData.hasGameStarted) {
       return (
         <div className="Action-panel">
-          <div>
+          <div className="Reset-btn-div">
             <button type="submit" value="Reset Game" onClick={this.handleReset}>Reset Game</button> 
           </div>
-          <div className="div-vertical-btns">
-            <button type="submit" onClick={() => this.changeWagerNum('increase')}>+</button>
-            <button type="submit" onClick={() => this.changeWagerNum('decrease')}>-</button>
-          </div>
-          <this.GameDice num = { this.state.currentNum ? this.state.currentNum : 1 } />
-          <div>
-            { this.state.currentQty }
-          </div>
+          <WagerInput 
+            messagesSender = {this.props.messagesSender} 
+            gameData = {this.props.gameData} 
+          />
         </div>
       );
     } else {
---
+++ client/src/components/WagerInput.tsx
@@ -0,0 +1,116 @@
+import React from 'react'; 
+import Message from '../interfaces/Message';
+import GameData from '../interfaces/GameData';
+
+type myProps = {
+  messagesSender: any,
+  gameData: GameData
+};
+
+type myState = {
+  minDiceNum: number,
+  minDiceQty: number,
+  currentNum: number,
+  currentQty: number,
+};
+
+export default class WagerInput extends React.Component<myProps, myState> {
+  constructor(props: myProps) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeWagerNum = this.changeWagerNum.bind(this);
+    this.state = { minDiceNum: 0, minDiceQty: 0, currentNum: 0, currentQty: 0 }
+  }
+
+  componentDidMount() {
+    if (this.props.gameData.lastWager.num && this.props.gameData.lastWager.qty) {
+      let minNum = this.props.gameData.lastWager.num !== 6 ? this.props.gameData.lastWager.num + 1 : 1;
+      let minQty = this.props.gameData.lastWager.num !== 6 ? this.props.gameData.lastWager.qty : this.props.gameData.lastWager.qty + 1;
+      
+      this.setState({ 
+        minDiceNum: minNum,
+        minDiceQty: minQty,
+        currentNum: minNum, 
+        currentQty: minQty
+      });
+    } else {
+      this.setState({ 
+        minDiceNum: 1, 
+        minDiceQty: 1,
+        currentNum: 1,
+        currentQty: 1
+      });
+    }
+  }
+
+  private handleSubmit() {
+    const message : Message = { type: 'action', name: 'start game', payload: {} };
+    this.props.messagesSender(message);
+  }
+
+  private changeWagerNum(direction: 'increase' | 'decrease') {
+    let currentNum = this.state.currentNum;
+    let currentQty = this.state.currentQty;
+    let minQty = this.state.minDiceQty;
+    let minNum = this.state.minDiceNum;
+    let diceRemaining = this.props.gameData.numDiceRemaining;
+
+    if (direction==='increase') {
+      switch (true) {
+        case currentNum < 6:
+          this.setState({currentNum: currentNum + 1});
+          break;
+        case currentNum === 6:
+          // Allow the + button to rollover from 6 to 1, but increase the wager qty
+          // Only allow legal wagers
+          if (currentQty < diceRemaining) {
+            this.setState({ currentNum: 1, currentQty: currentQty + 1 });
+          }
+          break;
+      }
+    } else {
+      switch (true) {
+        case currentNum > 1:
+          //Only allow legal wagers
+          if (currentNum > minNum && currentQty >= minQty) {
+            this.setState({ currentNum: currentNum - 1 });
+          }
+          break;
+        case currentNum === 1:
+          // Allow the - button to rollowver from 1 to 6, but decrease the wager qty
+          // Only allow legal wagers
+          if (minQty < currentQty) {
+            this.setState({ currentNum: 6, currentQty: currentQty - 1 });
+          }
+          break;
+      }
+    }
+  }
+
+  private GameDice = (props: { num: Number }) => {
+    if (props.num===0) { return(<span></span>); }
+    return (
+      <img 
+        src={"../images/dice"+ props.num.toString() + ".svg"} 
+        alt={props.num.toString()} 
+        height="40px" 
+        className="img-dice">
+      </img>
+    );
+  }
+
+  render() {
+    return (
+      <div className="Wager-input">
+        <div className="div-vertical-btns">
+          <button type="submit" onClick={() => this.changeWagerNum('increase')}>+</button>
+          <button type="submit" onClick={() => this.changeWagerNum('decrease')}>-</button>
+        </div>
+        <this.GameDice num = { this.state.currentNum ? this.state.currentNum : 1 } />
+        <div>
+          { this.state.currentQty }
+        </div>
+      </div>
+    );
+  }
+}
