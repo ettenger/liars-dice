282c5aa53d5a39cb22b9c6e65cce5071ac919266
Eric <Eric@Erics-MacBook-Pro.local>
2020-04-14 22:45:47 -0400

    Dice now displayed at end of round

--- client/src/components/GameLog.tsx
+++ client/src/components/GameLog.tsx
@@ -1,9 +1,14 @@
-import React from 'react'; 
+import React, { Fragment } from 'react'; 
 import Message from '../interfaces/Message'
 import Wager from '../interfaces/Wager';
 
 type myProps = {
   messages: Message[]
+};
+
+type diceReveal = {
+  name: string,
+  dice: number[]
 };
 
 export default class GameLog extends React.Component<myProps> {
@@ -14,38 +19,68 @@ export default class GameLog extends React.Component<myProps> {
     if (props.message.type === 'action') {
       switch (props.message.name) {
         case 'player join':
-          returnVal = <span style={{color: '#9b9b9b'}}>{props.message.payload} joined the game<br/></span>;
+          returnVal = <span style={{display: 'flex', color: '#9b9b9b'}}>{props.message.payload} joined the game<br/></span>;
           break;
         case 'game start':
-          returnVal = <span style={{color: 'cyan'}}>The game is starting!<br/></span>;
+          returnVal = <span style={{display: 'flex', color: 'cyan'}}>The game is starting!<br/></span>;
           break;
         case 'wager':
           returnVal = 
-            <span>
-              <span style={{color: '#ffc400'}}>{props.message.payload.player}: </span>
+            <span style={{display: 'flex'}}>
+              <span style={{color: '#ffc400'}}>{props.message.payload.player}:&nbsp;</span>
               <span style={{color: 'white'}}>{this.formatWager(props.message.payload.wager)}<br/></span>
             </span>
           break;
         case 'dice reveal':
           const tempWager: Wager = { callBullshit: false, num: props.message.payload.num, qty: props.message.payload.count}
-          returnVal = <span style={{color: '#d842a6'}}>There {props.message.payload.count !== 1 ? 'are':'is'} {this.formatWager(tempWager)}<br/></span>
+          let allDice: Array<diceReveal> = props.message.payload.dice;
+
+          returnVal = 
+            <>
+              { allDice.map((dice, key) => (
+                <Fragment key={key}>
+                  <span style={{display: 'flex', alignItems: 'center'}}>
+                    <span style={{color: '#ffc400'}}>{dice.name}:&nbsp;</span>
+                    { dice.dice.sort().map((num, index) =>
+                      <this.GameDice
+                        key={index}
+                        num={num}
+                      />,
+                    )}
+                  <br/></span>
+                </Fragment>
+              )) }
+              <span style={{display: 'flex', color: '#d842a6'}}>There {props.message.payload.count !== 1 ? 'are':'is'} {this.formatWager(tempWager)}<br/></span>
+            </>
           break;
         case 'lose die':
-          returnVal = <span style={{color: '#d842a6'}}>{props.message.payload} loses a die<br/></span>
+          returnVal = <span style={{display: 'flex', color: '#d842a6'}}>{props.message.payload} loses a die<br/></span>
           break;
         case 'round start':
-          returnVal = <span style={{color: 'cyan'}}>Starting a new round!<br/></span>;
+          returnVal = <span style={{display: 'flex', color: 'cyan'}}>Starting a new round!<br/></span>;
           break;
         case 'player eliminated':
-          returnVal = <span style={{color: '#ff4d4d'}}>{props.message.payload} was eliminated!<br/></span>;
+          returnVal = <span style={{display: 'flex', color: '#ff4d4d'}}>{props.message.payload} was eliminated!<br/></span>;
           break;
         case 'game over':
-          returnVal = <span style={{color: '#299129'}}>The game is over. {props.message.payload} wins!<br/></span>;
+          returnVal = <span style={{display: 'flex', color: '#299129'}}>The game is over. {props.message.payload} wins!<br/></span>;
           break;
       }
     } 
 
     return returnVal;
+  }
+
+  private GameDice = (props: { key: any, num: Number }) => {
+    if (props.num===0) { return(<span></span>); }
+    return (
+      <img 
+        src={"../images/dice"+ props.num.toString() + ".svg"} 
+        alt={props.num.toString()} 
+        height="20px" 
+        className="img-dice">
+      </img>
+    );
   }
 
   private formatWager(wager: Wager) {
